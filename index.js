04d2efa97853546b7a821572ea355f42c1f5e0d4
Quade Houston <quadehouston@gmail.com>
2022-07-18 22:11:42 -0400

    commit tree 5748057bad23deb67a98d8a8c6167e7f8e21082e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (activity) {
+    if (activity) {
+        return `This Monday, I will ${activity}.`
+    }
+    else return `This Monday, I will go to the office.`
+}
+
+function wrapAdjective (flare = '*') {
+    return function (state = 'special') {
+        return `You are ${flare}${state}${flare}!`
+    }
+}
